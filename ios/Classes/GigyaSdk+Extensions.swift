db2d40e81654de548e31a03a40035e453f6d4172
Sagi <sagi.shmuel@sap.com>
2020-11-30 10:56:22 +0200

    #127833 - add `showScreenset` to plugin & iOS

---
+++ ios/Classes/GigyaSdk+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  GigyaSdk+Extensions.swift
+//  gigya_flutter_plugin
+//
+//  Created by Shmuel, Sagi on 26/11/2020.
+//
+
+import Foundation
---
+++ ios/Classes/ScreenSetsStreamHandler.swift
@@ -0,0 +1,8 @@
+//
+//  ScreenSetsStreamHandler.swift
+//  gigya_flutter_plugin
+//
+//  Created by Shmuel, Sagi on 26/11/2020.
+//
+
+import Foundation
