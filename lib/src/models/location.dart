dfbc2d773bc40e723e50c268a4a71914550b0038
Navaron Bracke <brackenavaron@gmail.com>
2022-11-27 16:54:45 +0100

    move location to different file

---
+++ lib/src/models/location.dart
@@ -0,0 +1,79 @@
+/// This class represents a location.
+class Location {
+  /// The private constructor.
+  Location._({
+    this.city,
+    this.coordinates,
+    this.country,
+    this.state,
+  });
+
+  /// The default constructor.
+  factory Location.fromJson(Map<String, dynamic> json) {
+    final Map<String, dynamic>? jsonCoordinates =
+        json['coordinates'] as Map<String, dynamic>?;
+
+    final double? latitude = jsonCoordinates?['lat'] as double?;
+    final double? longitude = jsonCoordinates?['lon'] as double?;
+
+    Coordinates? coordinates;
+
+    if (latitude != null && longitude != null) {
+      coordinates = Coordinates(latitude: latitude, longitude: longitude);
+    }
+
+    return Location._(
+      city: json['city'] as String?,
+      coordinates: coordinates,
+      country: json['country'] as String?,
+      state: json['state'] as String?,
+    );
+  }
+
+  /// The name of the city at the given [coordinates].
+  final String? city;
+
+  /// The coordinates of the location.
+  final Coordinates? coordinates;
+
+  /// The name of the country at the given [coordinates].
+  final String? country;
+
+  /// The name of the state within the [country] to which the [city] belongs to.
+  final String? state;
+
+  /// Convert this object to a JSON object.
+  Map<String, dynamic> toJson() {
+    final Coordinates? latLng = coordinates;
+
+    return <String, dynamic>{
+      'city': city,
+      if (latLng != null) 'coordinates': latLng.toJson(),
+      'country': country,
+      'state': state,
+    };
+  }
+}
+
+/// This class represents a set of coordinates.
+class Coordinates {
+  /// The default constructor.
+  Coordinates({
+    required this.latitude,
+    required this.longitude,
+  });
+
+  /// The latitude of the coordinate.
+  final double latitude;
+
+  /// The longitude of the coordinate.
+  final double longitude;
+
+  /// Convert the coordinates to a JSON object.
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'lat': latitude,
+      'lon': longitude,
+    };
+  }
+}
