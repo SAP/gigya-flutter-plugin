5111b64e9527009570fd2f1e29898cab7587713d
Navaron Bracke <brackenavaron@gmail.com>
2022-11-27 17:08:27 +0100

    move address into a different file

---
+++ lib/src/models/address.dart
@@ -0,0 +1,54 @@
+/// This class represents an address.
+class Address {
+  /// The private constructor.
+  Address._({
+    this.country,
+    this.formatted,
+    this.locality,
+    this.postalCode,
+    this.region,
+    this.streetAddress,
+  });
+
+  /// The default constructor.
+  factory Address.fromJson(Map<String, dynamic> json) {
+    return Address._(
+      country: json['country'] as String?,
+      formatted: json['formatted'] as String?,
+      locality: json['locality'] as String?,
+      postalCode: json['postal_code'] as String?,
+      region: json['region'] as String?,
+      streetAddress: json['street_address'] as String?,
+    );
+  }
+
+  /// The country in which the address resides.
+  final String? country;
+
+  /// The formatted address.
+  final String? formatted;
+
+  /// The locality of the address.
+  final String? locality;
+
+  /// The postal code of the address.
+  final String? postalCode;
+
+  /// The name of the region in which the address resides.
+  final String? region;
+
+  /// The street address of the address.
+  final String? streetAddress;
+
+  /// Convert this object into a JSON object.
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'country': country,
+      'formatted': formatted,
+      'locality': locality,
+      'postal_code': postalCode,
+      'region': region,
+      'street_address': streetAddress,
+    };
+  }
+}
