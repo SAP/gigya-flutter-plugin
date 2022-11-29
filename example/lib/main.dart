dcf05cbad430d8f03d6acc28d1172033e4eb6670
Navaron Bracke <brackenavaron@gmail.com>
2022-11-29 16:03:44 +0100

    initialize the sdk in the example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gigya_flutter_plugin/gigya_flutter_plugin.dart';
 import 'package:gigya_flutter_plugin_example/routes/account_information.dart';
 import 'package:gigya_flutter_plugin_example/routes/forgot_password.dart';
 import 'package:gigya_flutter_plugin_example/routes/home_page.dart';
@@ -8,12 +9,29 @@ import 'package:gigya_flutter_plugin_example/routes/manage_connection.dart';
 import 'package:gigya_flutter_plugin_example/routes/register_with_credentials.dart';
 import 'package:gigya_flutter_plugin_example/routes/send_request.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  final GigyaSdk sdk = const GigyaSdk();
+
+  try {
+    await sdk.initSdk(
+      apiDomain: 'your_domain',
+      apiKey: 'your_api_key',
+    );
+  } catch (error) {
+    print(error);
+  }
+
+  runApp(MyApp(sdk));
 }
 
 /// The example app.
 class MyApp extends StatefulWidget {
+  /// The default constructor.
+  const MyApp(this.sdk);
+
+  /// The initialized Gigya SDK.
+  final GigyaSdk sdk;
+
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -24,14 +42,14 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       initialRoute: '/',
       routes: <String, WidgetBuilder>{
-        '/': (_) => HomePageWidget(),
-        '/send_request': (_) => SendRequestPageWidget(),
-        '/login_credentials': (_) => LoginWidthCredentialsWidget(),
-        '/register_email': (_) => RegisterWithEmailWidget(),
-        '/account_information': (_) => AccountInformationWidget(),
-        '/manage_connections': (_) => ManageConnectionWidget(),
-        '/forgot_password': (_) => ForgotPasswordPageWidget(),
-        '/otp_phone_login': (_) => OTPLoginWidget(),
+        '/': (_) => HomePageWidget(widget.sdk),
+        '/send_request': (_) => SendRequestPageWidget(widget.sdk),
+        '/login_credentials': (_) => LoginWidthCredentialsWidget(widget.sdk),
+        '/register_email': (_) => RegisterWithEmailWidget(widget.sdk),
+        '/account_information': (_) => AccountInformationWidget(widget.sdk),
+        '/manage_connections': (_) => ManageConnectionWidget(widget.sdk),
+        '/forgot_password': (_) => ForgotPasswordPageWidget(widget.sdk),
+        '/otp_phone_login': (_) => OTPLoginWidget(widget.sdk),
       },
     );
   }
