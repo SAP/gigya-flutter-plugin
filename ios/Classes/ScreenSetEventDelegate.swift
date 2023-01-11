274f7e6176bc31bdd4e9f0e7a441e47893f2a5b4
Navaron Bracke <brackenavaron@gmail.com>
2023-01-11 15:05:53 +0100

    use FlutterError for error events in Swift

--- ios/Classes/ScreenSetEventDelegate.swift
+++ ios/Classes/ScreenSetEventDelegate.swift
@@ -1,7 +1,9 @@
+import Flutter
+
 /// This interface defines a handler for screen set events.
 protocol ScreenSetEventDelegate {
     /// Add an error event to the event sink.
-    func addScreenSetError(errorCode: String, errorMessage: String, errorDetails: Any?) -> Void
+    func addScreenSetError(error: FlutterError) -> Void
 
     /// Add the given event to the event sink.
     func addScreenSetEvent(event: [String, Any?]) -> Void
