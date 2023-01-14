d7137aa134df3c441b35d3047f1fafaa2d52ea76
Navaron Bracke <brackenavaron@gmail.com>
2023-01-14 13:21:36 +0100

    don't cache registrar in Swift

--- ios/Classes/SwiftGigyaFlutterPlugin.swift
+++ ios/Classes/SwiftGigyaFlutterPlugin.swift
@@ -2,21 +2,12 @@ import Flutter
 import UIKit
 import Gigya
 
-public class SwiftGigyaFlutterPlugin: NSObject, FlutterPlugin {
-    /// A reference to the plugin registry
-    /// that is used to register a ``SwiftGigyaFlutterPluginTyped`` instance
-    /// with a specific ``GigyaAccountProtocol``.
-    ///
-    /// This reference is discarded after registration is complete.
-    static var registrar: FlutterPluginRegistrar?
-    
-    /// The ``FlutterPlugin.register()`` implementation of this plugin
-    /// only saves a reference to the plugin registry.
-    ///
-    /// To actually register an instance of the plugin,
-    /// call the ``registerForProtocol()`` method in your AppDelegate implementation.
+public class SwiftGigyaFlutterPlugin: NSObject, FlutterPlugin {    
     public static func register(with registrar: FlutterPluginRegistrar) {
-        self.registrar = registrar
+       // This method is a stub.
+       // Register the plugin by calling
+       // `SwiftGigyaFlutterPlugin.registerForProtocol(accountSchema: accountSchema, registrar: self)`
+       // in your ApplicationDelegate implementation.
     }
     
     /// Register this plugin with Flutter's plugin registry.
@@ -27,11 +18,7 @@ public class SwiftGigyaFlutterPlugin: NSObject, FlutterPlugin {
     /// This schema is passed to the Gigya SDK during setup.
     ///
     /// See also: https://github.com/SAP/gigya-swift-sdk/blob/main/GigyaSwift/Models/User/GigyaAccount.swift
-    public static func registerForProtocol<T: GigyaAccountProtocol>(accountSchema: T.Type) {
-        guard let registrar = self.registrar else { return }
-
-        self.registrar = nil // Discard the plugin registry reference.
-
+    public static func registerForProtocol<T: GigyaAccountProtocol>(accountSchema: T.Type, registrar: FlutterPluginRegistrar) {
         let instance = SwiftGigyaFlutterPluginTyped(accountSchema: accountSchema)
         instance.register(with: registrar)
     }
