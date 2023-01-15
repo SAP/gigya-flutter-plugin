469d291aa08dc0ac389182b260893a55fcfb3a43
Navaron Bracke <brackenavaron@gmail.com>
2023-01-15 13:42:54 +0100

    restore the social providers in the example app on iOS

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 import Flutter
 import gigya_flutter_plugin
 import Gigya
+import FBSDKCoreKit
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -11,10 +12,26 @@ import Gigya
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     SwiftGigyaFlutterPlugin.register(accountSchema: GigyaAccount.self)
-
-    // Register social providers here, i.e. 
-    // Gigya.sharedInstance(GigyaAccount.self).registerSocialProvider(of: .facebook, wrapper: FacebookWrapper())
+    
+    // Register social providers here.
+    Gigya.sharedInstance(GigyaAccount.self).registerSocialProvider(of: .facebook, wrapper: FacebookWrapper())
+    Gigya.sharedInstance(GigyaAccount.self).registerSocialProvider(of: .google, wrapper: GoogleWrapper())
+    
+    ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    override func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+        ApplicationDelegate.shared.application(
+            app,
+            open: url,
+            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+            annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+        )
+    }
 }
