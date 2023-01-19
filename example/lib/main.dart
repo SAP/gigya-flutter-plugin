c913b30a58e4740018a4ec44668778713f13fd83
Navaron Bracke <brackenavaron@gmail.com>
2023-01-19 19:32:47 +0100

    reimplement register page

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:gigya_flutter_plugin/gigya_flutter_plugin.dart';
 
 import 'routes/home_page.dart';
 import 'routes/login_with_credentials_page.dart';
+import 'routes/register_with_email_page.dart';
 import 'routes/send_request_page.dart';
 
 void main() async {
@@ -43,6 +44,7 @@ class _MyAppState extends State<MyApp> {
         '/': (_) => HomePage(sdk: widget.sdk),
         '/send_request': (_) => SendRequestPage(sdk: widget.sdk),
         '/login_credentials': (_) => LoginWithCredentialsPage(sdk: widget.sdk),
+        '/register_email': (_) => RegisterWithEmailPage(sdk: widget.sdk),
       },
     );
   }
