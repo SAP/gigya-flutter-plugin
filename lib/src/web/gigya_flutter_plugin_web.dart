337baf34e52285cb65be55aa5879bebad7f7682c
Navaron Bracke <brackenavaron@gmail.com>
2023-03-14 14:49:13 +0100

    implement initSdk() for web

--- lib/src/web/gigya_flutter_plugin_web.dart
+++ lib/src/web/gigya_flutter_plugin_web.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+import 'dart:html' as html;
+
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 import '../platform_interface/gigya_flutter_plugin_platform_interface.dart';
+import 'gigya_js_interop.dart';
 
 /// An implementation of [GigyaFlutterPluginPlatform] that uses JavaScript static interop.
 class GigyaFlutterPluginWeb extends GigyaFlutterPluginPlatform {
@@ -11,5 +15,39 @@ class GigyaFlutterPluginWeb extends GigyaFlutterPluginPlatform {
     GigyaFlutterPluginPlatform.instance = GigyaFlutterPluginWeb();
   }
 
-  // TODO: implement the platform interface
+  @override
+  Future<void> initSdk({
+    required String apiDomain,
+    required String apiKey,
+    bool forceLogout = true,
+  }) async {
+    final Completer<void> onGigyaServiceReadyCompleter = Completer<void>();
+    final JSWindow domWindow = html.window as JSWindow;
+
+    // Set `window.onGigyaServiceReady` before creating the script.
+    // That function is called when the SDK has been initialized.
+    domWindow.onGigyaServiceReady = () {
+      if (!onGigyaServiceReadyCompleter.isCompleted) {
+        onGigyaServiceReadyCompleter.complete();
+      }
+    };
+
+    final html.ScriptElement script = html.ScriptElement()
+      ..async = true
+      ..defer = false
+      ..type = 'text/javascript'
+      ..lang = 'javascript'
+      ..crossOrigin = 'anonymous'
+      ..src = 'cdns.$apiDomain/js/gigya.js?apikey=$apiKey';
+
+    html.document.head!.append(script);
+
+    await script.onLoad.first;
+
+    if (forceLogout) {
+      await logout();
+    }
+
+    return onGigyaServiceReadyCompleter.future;
+  }
 }
