f456791cef17956a1535d3d9e270a5214973291f
Navaron Bracke <brackenavaron@gmail.com>
2023-03-16 13:20:44 +0100

    implement emails static interop class

---
+++ lib/src/web/static_interop/emails.dart
@@ -0,0 +1,19 @@
+import 'package:js/js.dart';
+
+/// The static interop class for the emails object
+/// that is nested within a profile object.
+@JS()
+@anonymous
+class Emails {
+  /// Create a new [Emails] instance.
+  external factory Emails({
+    List<dynamic> unverified,
+    List<dynamic> verified,
+  });
+
+  /// The list of unverified email addresses.
+  external List<dynamic> get unverified;
+
+  /// The list of verified email addresses.
+  external List<dynamic> get verified;
+}
