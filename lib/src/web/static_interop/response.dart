0307289ad8e03cca7ef42ca9c81c36d9b84aa8ce
Navaron Bracke <brackenavaron@gmail.com>
2023-03-16 13:31:09 +0100

    add remaining properties for login response static interop implementation

--- lib/src/web/static_interop/response.dart
+++ lib/src/web/static_interop/response.dart
@@ -1,5 +1,9 @@
 import 'package:js/js.dart';
 
+import 'emails.dart';
+import 'profile.dart';
+import 'session_info.dart';
+
 /// The static interop class for the Gigya `Response` class.
 ///
 /// See also: https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/416d55f070b21014bbc5a10ce4041860.html
@@ -61,6 +65,12 @@ class LoginResponse {
   /// The unique identifier of the transaction.
   external String get callId;
 
+  /// The timestamp of the creation of the user.
+  external String? get createdTimestamp;
+
+  /// The verified and unverified email addresses of the user.
+  external Emails get emails;
+
   /// The response error code.
   ///
   /// A value of zero indicates success.
@@ -74,4 +84,51 @@ class LoginResponse {
 
   /// The error message for the given [errorCode].
   external String? get errorMessage;
+
+  /// Whether the user is active.
+  external bool? get isActive;
+
+  /// Whether the user is registered.
+  external bool? get isRegistered;
+
+  /// Whether the user is verified.
+  external bool? get isVerified;
+
+  /// The timestamp of the lat login of the user.
+  external String? get lastLogin;
+
+  /// The timestamp of the last update to the user's profile,
+  /// preferences or subscriptions data.
+  external String? get lastUpdated;
+
+  /// The login provider that was last used to log in.
+  external String? get loginProvider;
+
+  /// The timestamp, when the oldest data of the user was refreshed.
+  external String? get oldestDataUpdated;
+
+  /// The user's profile.
+  external Profile? get profile;
+
+  /// The timestamp when the user was registered.
+  external String? get registered;
+
+  /// The session info for the user's session.
+  external SessionInfo get sessionInfo;
+
+  /// The timestamp of the [UIDSignature].
+  /// This value can be a [String] or a [num].
+  external Object? get signatureTimestamp;
+
+  /// The comma separated list of social providers linked to this user.
+  external String? get socialProviders;
+
+  /// The unique id of this user.
+  external String? get UID; // ignore: non_constant_identifier_names
+
+  /// The verification signature of the [UID].
+  external String? get UIDSignature; // ignore: non_constant_identifier_names
+
+  /// The timestamp when the user was verified.
+  external String? get verified;
 }
