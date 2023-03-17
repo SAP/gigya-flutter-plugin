09e163ba1c443a283aefa081f6678bfed04a70e8
Navaron Bracke <brackenavaron@gmail.com>
2023-03-17 12:51:11 +0100

    convert emails to static interop

--- lib/src/web/static_interop/emails.dart
+++ lib/src/web/static_interop/emails.dart
@@ -3,13 +3,18 @@ import 'package:js/js.dart';
 /// The static interop class for the emails object.
 @JS()
 @anonymous
+@staticInterop
 class Emails {
   /// Create a new [Emails] instance.
   external factory Emails({
     List<dynamic> unverified,
     List<dynamic> verified,
   });
+}
 
+/// This extension defines the static interop definition
+/// for the [Emails] class.
+extension EmailsExtension on Emails {
   /// The list of unverified email addresses.
   external List<dynamic> get unverified;
 
