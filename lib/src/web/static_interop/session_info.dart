b396f3b826ab78b656eee5a26d096ed3bd978079
Navaron Bracke <brackenavaron@gmail.com>
2023-03-17 13:19:06 +0100

    convert session info to static interop

--- lib/src/web/static_interop/session_info.dart
+++ lib/src/web/static_interop/session_info.dart
@@ -3,13 +3,18 @@ import 'package:js/js.dart';
 /// The static interop class for the session info object.
 @JS()
 @anonymous
+@staticInterop
 class SessionInfo {
   /// Construct a new [SessionInfo] instance.
   external factory SessionInfo({
     String? cookieName,
     String? cookieValue,
   });
+}
 
+/// This extension defines the static interop definition
+/// for the [SessionInfo] class.
+extension SessionInfoExtension on SessionInfo {
   /// The name of the session cookie.
   external String? get cookieName;
 
