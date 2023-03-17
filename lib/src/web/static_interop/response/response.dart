7e61f4b454ab9138397a4cbb85ee8bcf6ebf5d49
Navaron Bracke <brackenavaron@gmail.com>
2023-03-17 15:32:40 +0100

    export response classes in response.dart

--- lib/src/web/static_interop/response/response.dart
+++ lib/src/web/static_interop/response/response.dart
@@ -1,5 +1,11 @@
 import 'package:js/js.dart';
 
+// Export the response classes,
+// since importing one does not include the extension getters,
+// which are defined here.
+export 'conflicting_account_response.dart';
+export 'login_response.dart';
+
 /// The static interop class for the Gigya `Response` class.
 ///
 /// See also: https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/416d55f070b21014bbc5a10ce4041860.html
