aac2481285bd12ecd9390df7c66dd165690f7498
Navaron Bracke <brackenavaron@gmail.com>
2023-04-12 13:46:35 +0200

    add support for SAML social provider and other social providers

--- lib/src/models/enums/social_provider.dart
+++ lib/src/models/enums/social_provider.dart
@@ -1,26 +1,112 @@
+// TODO: investigate if this can be a `final class` once Class Modifiers are available in Dart 3.0
+// https://github.com/dart-lang/language/blob/master/accepted/future-releases/class-modifiers/feature-specification.md#syntax
+
 /// The currently supported social providers.
-enum SocialProvider {
+///
+/// Depending on the platform; some social providers might be unavailable.
+class SocialProvider {
+  /// The private constructor.
+  const SocialProvider._(this.name);
+
+  /// Create a new SAML-based [SocialProvider] from a given [value].
+  ///
+  /// The [value] will be prefixed by `saml-`.
+  factory SocialProvider.saml(String value) {
+    if (value.isEmpty) {
+      throw ArgumentError.value(
+        value,
+        'value',
+        'The value of a SAML SocialProvider should not be empty.',
+      );
+    }
+
+    return SocialProvider._('saml-$value');
+  }
+
+  /// The name of this provider.
+  final String name;
+
   /// The social provider for Amazon.
-  amazon,
+  static const SocialProvider amazon = SocialProvider._('amazon');
 
   /// The social provider for Apple.
-  apple,
+  static const SocialProvider apple = SocialProvider._('apple');
+
+  /// The social provider for Blogger.
+  static const SocialProvider blogger = SocialProvider._('blogger');
 
   /// The social provider for Facebook.
-  facebook,
+  static const SocialProvider facebook = SocialProvider._('facebook');
+
+  /// The social provider for Foursquare.
+  static const SocialProvider foursquare = SocialProvider._('foursquare');
 
   /// The social provider for Google.
-  google,
+  static const SocialProvider google = SocialProvider._('google');
+
+  /// The social provider for Google+.
+  static const SocialProvider googleplus = SocialProvider._('googleplus');
+
+  /// The social provider for Kakao.
+  static const SocialProvider kakao = SocialProvider._('kakao');
 
   /// The social provider for Line.
-  line,
+  static const SocialProvider line = SocialProvider._('line');
 
   /// The social provider for LinkedIn.
-  linkedin,
+  static const SocialProvider linkedin = SocialProvider._('linkedin');
+
+  /// The social provider for Livedoor.
+  static const SocialProvider livedoor = SocialProvider._('livedoor');
+
+  /// The social provider for Facebook Messenger.
+  static const SocialProvider messenger = SocialProvider._('messenger');
+
+  /// The social provider for Mixi.
+  static const SocialProvider mixi = SocialProvider._('mixi');
+
+  /// The social provider for Naver.
+  static const SocialProvider naver = SocialProvider._('naver');
+
+  /// The social provider for Netlog.
+  static const SocialProvider netlog = SocialProvider._('netlog');
+
+  /// The social provider for Odnoklassniki.
+  static const SocialProvider odnoklassniki = SocialProvider._('odnoklassniki');
+
+  /// The social provider for Paypal.
+  static const SocialProvider paypal = SocialProvider._('paypaloauth');
+
+  /// The social provider for QQ.
+  static const SocialProvider qq = SocialProvider._('qq');
+
+  /// The social provider for Renren.
+  static const SocialProvider renren = SocialProvider._('renren');
+
+  /// The social provider for Sina.
+  static const SocialProvider sina = SocialProvider._('sina');
+
+  /// The social provider for Spiceworks.
+  static const SocialProvider spiceworks = SocialProvider._('spiceworks');
+
+  /// The social provider for Twitter.
+  static const SocialProvider twitter = SocialProvider._('twitter');
+
+  /// The social provider for VKontakte.
+  static const SocialProvider vkontakte = SocialProvider._('vkontakte');
 
   /// The social provider for WeChat.
-  wechat,
+  static const SocialProvider wechat = SocialProvider._('wechat');
 
-  /// The soocial provider for Yahoo.
-  yahoo,
+  /// The social provider for WordPress.
+  static const SocialProvider wordpress = SocialProvider._('wordpress');
+
+  /// The social provider for Xing.
+  static const SocialProvider xing = SocialProvider._('xing');
+
+  /// The social provider for Yahoo.
+  static const SocialProvider yahoo = SocialProvider._('yahoo');
+
+  /// The social provider for Yahoo Japan.
+  static const SocialProvider yahooJapan = SocialProvider._('yahoojapan');
 }
