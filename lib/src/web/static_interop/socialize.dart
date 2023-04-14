0961c023bcf64af82d2bd3e56f038debcbaba12a
Navaron Bracke <brackenavaron@gmail.com>
2023-04-14 08:54:50 +0200

    implement add social connection method static interop definition

--- lib/src/web/static_interop/socialize.dart
+++ lib/src/web/static_interop/socialize.dart
@@ -1,8 +1,18 @@
 import 'dart:js_interop';
 
+import 'parameters/social_connection.dart';
+
 /// The extension type for the `gigya.socialize` JavaScript object.
 ///
 /// See also: https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/4173357f70b21014bbc5a10ce4041860.html
 @JS()
 @staticInterop
-extension type Socialize(JSObject _) {}
+extension type Socialize(JSObject _) {
+  /// Add a new social connection.
+  /// 
+  /// This function receives a [AddSocialConnectionParameters] argument,
+  /// and has [JSVoid] as return type.
+  external JSFunction addConnection;
+
+  // TODO: remove connection  
+}
