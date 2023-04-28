fe52143daa26bcc0a9310893e5f1da88cbad513b
Navaron Bracke <brackenavaron@gmail.com>
2023-04-28 15:30:23 +0200

    implement show screenset (basic parameters + global events)

--- lib/src/web/gigya_flutter_plugin_web.dart
+++ lib/src/web/gigya_flutter_plugin_web.dart
@@ -6,12 +6,15 @@ import 'package:js/js_util.dart' show allowInterop;
 import 'package:web/web.dart' as web;
 
 import '../models/gigya_error.dart';
+import '../models/screenset_event.dart';
 import '../platform_interface/gigya_flutter_plugin_platform_interface.dart';
 import '../services/interruption_resolver.dart';
 import 'enums/web_error_code.dart';
 import 'static_interop/gigya_web_sdk.dart';
+import 'static_interop/parameters/add_event_handlers_parameters.dart';
 import 'static_interop/parameters/basic.dart';
 import 'static_interop/parameters/login.dart';
+import 'static_interop/parameters/screenset_parameters.dart';
 import 'static_interop/response/response.dart';
 import 'static_interop/window.dart';
 import 'web_interruption_resolver.dart';
@@ -208,5 +211,72 @@ class GigyaFlutterPluginWeb extends GigyaFlutterPluginPlatform {
     );
 
     return completer.future;
+  }
+
+  @override
+  Stream<ScreensetEvent> showScreenSet(
+    String name, {
+    Map<String, dynamic> parameters = const <String, dynamic>{},
+  }) {
+    late StreamController<ScreensetEvent> controller;
+
+    controller = StreamController<ScreensetEvent>.broadcast(onListen: () {
+      // First setup the handlers for the global events.
+      GigyaWebSdk.instance.accounts.addEventHandlers(AddEventHandlersParameters(
+        onAfterResponse: allowInterop((Object? response) {
+          if (response == null || controller.isClosed) {
+            return;
+          }
+
+          final Map<Object?, Object?> eventData = dartify(response) as Map<Object?, Object?>;
+
+          controller.add(
+            ScreensetEvent(
+              eventData['eventName'] as String? ?? 'onAfterResponse',
+              eventData.cast<String, Object?>(),
+            ),
+          );
+        }),
+        onLogin: allowInterop((LoginGlobalEventResponse response) {
+          if (controller.isClosed) {
+            return;
+          }
+
+          controller.add(ScreensetEvent('onLogin', response.toMap()));
+        }),
+      ));
+
+      GigyaWebSdk.instance.accounts.showScreenSet(
+        ShowScreensetParameters(
+          // Basic parameters first.
+          authFlow: parameters['authFlow'] as String? ?? 'popup',
+          communicationLangByScreenSet: parameters['communicationLangByScreenSet'] as bool? ?? true,
+          deviceType: parameters['deviceType'] as String? ?? 'desktop',
+          dialogStyle: parameters['dialogStyle'] as String? ?? 'modern',
+          enabledProviders: parameters['enabledProviders'] as String?,
+          googlePlayAppID: parameters['googlePlayAppID'] as String?,
+          lang: parameters['lang'] as String?,
+          mobileScreenSet: parameters['mobileScreenSet'] as String?,
+          redirectMethod: parameters['redirectMethod'] as String? ?? 'get',
+          redirectURL: parameters['redirectURL'] as String?,
+          regSource: parameters['regSource'] as String?,
+          regToken: parameters['regToken'] as String?,
+          screenSet: name,
+          sessionExpiration: parameters['sessionExpiration'] as int?,
+          startScreen: parameters['startScreen'] as String?,
+        ),
+      );
+    }, onCancel: () {
+      // Unset the handlers for the global events by setting them to null.
+      GigyaWebSdk.instance.accounts.addEventHandlers(AddEventHandlersParameters());
+
+      // Automatically hide the screenset when the last listener
+      // of the stream cancels its subscription.
+      GigyaWebSdk.instance.accounts.hideScreenSet(HideScreensetParameters(screenSet: name));
+
+      unawaited(controller.close());
+    });
+
+    return controller.stream;
   }
 }
