2463a16ec34501230552c33a276e09d4f17665cf
Navaron Bracke <brackenavaron@gmail.com>
2023-04-29 10:33:06 +0200

    implement screenset parameters (no handlers yet)

--- lib/src/web/static_interop/parameters/screenset_parameters.dart
+++ lib/src/web/static_interop/parameters/screenset_parameters.dart
@@ -10,3 +10,28 @@ class HideScreensetParameters {
   /// Construct a new [HideScreensetParameters] instance.
   external factory HideScreensetParameters({String screenSet});
 }
+
+/// This class represents the parameters for the `Accounts.showScreenset` method.
+@JS()
+@anonymous
+@staticInterop
+class ShowScreensetParameters {
+  /// Construct a new [ShowScreensetParameters] instance.
+  external factory ShowScreensetParameters({
+    String? authFlow,
+    bool? communicationLangByScreenSet,
+    String? deviceType,
+    String? dialogStyle,
+    String? enabledProviders,
+    String? googlePlayAppID,
+    String? lang,
+    String? mobileScreenSet,
+    String? redirectMethod,
+    String? redirectURL,
+    String? regSource,
+    String? regToken,
+    String screenSet,
+    int? sessionExpiration,
+    String? startScreen,
+  });
+}
