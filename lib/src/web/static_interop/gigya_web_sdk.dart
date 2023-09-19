52a1ac26598a9febc10efeacfde3fbe338e62edf
Navaron Bracke <brackenavaron@gmail.com>
2023-09-19 11:49:52 +0200

    convert the GigyaWebSdk JS definition to extension type; move 'window.gigya' to window.dart; add instance getter

--- lib/src/web/static_interop/gigya_web_sdk.dart
+++ lib/src/web/static_interop/gigya_web_sdk.dart
@@ -1,23 +1,20 @@
 import 'dart:js_interop';
 
 import 'package:js/js.dart';
+import 'package:web/web.dart' as web;
 
 import 'account.dart';
+import 'window.dart';
 
-/// Get the `gigya` JavaScript object.
-@JS('window.gigya')
-external GigyaWebSdk get gigyaWebSdk;
-
-/// The static interop class for the `gigya` JavaScript object.
+/// The static interop extension type for the `gigya` JavaScript object.
 ///
 /// See also: https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/417f6b5e70b21014bbc5a10ce4041860.html
 @JS()
 @staticInterop
-class GigyaWebSdk implements JSObject {}
+extension type GigyaWebSdk(JSObject _) implements JSObject {
+  /// Get the Gigya Web SDK instance from the window.
+  static GigyaWebSdk get instance => GigyaWindow(web.window).gigya;
 
-/// This extension defines the static interop definition
-/// for the [GigyaWebSdk] class.
-extension GigyaWebSdkExtension on GigyaWebSdk {
   /// Get the accounts namespace in the Gigya Web SDK.
   external Accounts get accounts;
 
