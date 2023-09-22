739e96145253acf193e2bbbabf17a97019c87750
Navaron Bracke <brackenavaron@gmail.com>
2023-09-22 15:44:11 +0200

    convert conflicting account response to extension type

--- lib/src/web/static_interop/response/conflicting_account_response.dart
+++ lib/src/web/static_interop/response/conflicting_account_response.dart
@@ -1,19 +1,15 @@
-import 'package:js/js.dart';
+import 'dart:js_interop';
 
 import '../models/conflicting_account.dart';
 import 'response.dart';
 
-/// The static interop class for the Gigya Conflicting Account API response.
+/// The extension type for the Gigya Conflicting Account API response.
 ///
 /// See also: https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/4134c49270b21014bbc5a10ce4041860.html
 @JS()
 @anonymous
 @staticInterop
-class ConflictingAccountResponse extends Response {}
-
-/// This extension defines the static interop definition
-/// for the [ConflictingAccountResponse] class.
-extension ConflictingAccountResponseExtension on ConflictingAccountResponse {
+extension type ConflictingAccountResponse(Response baseResponse) {
   /// Get the conflicting account that is included in the response.
   external JsConflictingAccount? get conflictingAccount;
 }
