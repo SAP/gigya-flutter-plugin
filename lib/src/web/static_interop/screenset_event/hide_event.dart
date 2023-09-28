b298ab317ff8e32ae7ab38b35b34499b37669831
Navaron Bracke <brackenavaron@gmail.com>
2023-09-28 13:55:33 +0200

    convert the hide event to extension type

--- lib/src/web/static_interop/screenset_event/hide_event.dart
+++ lib/src/web/static_interop/screenset_event/hide_event.dart
@@ -1,19 +1,15 @@
-import 'package:js/js.dart';
+import 'dart:js_interop';
 
 import '../../../models/enums/screen_set_event_type.dart';
 import '../../../models/screenset_event.dart';
 
-/// The static interop class for the hide event of the `Account.showScreenset` event stream.
+/// The extension type for the hide event of the `Account.showScreenset` event stream.
 ///
 /// See: https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/413a5b7170b21014bbc5a10ce4041860.html?locale=en-US#onhide-event-data
 @JS()
 @anonymous
 @staticInterop
-class HideEvent {}
-
-/// This extension defines the static interop definition
-/// for the [HideEvent] class.
-extension HideEventExtension on HideEvent {
+extension type HideEvent(JSObject _) {
   /// The name of the event.
   external String get eventName;
 
