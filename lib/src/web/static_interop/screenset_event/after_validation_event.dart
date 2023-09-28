fe42a8e6e6154035ce16efd1410083913b250848
Navaron Bracke <brackenavaron@gmail.com>
2023-09-28 13:48:44 +0200

    convert after validation event to extension type

--- lib/src/web/static_interop/screenset_event/after_validation_event.dart
+++ lib/src/web/static_interop/screenset_event/after_validation_event.dart
@@ -1,22 +1,17 @@
-import 'package:js/js.dart';
-import 'package:js/js_util.dart';
+import 'dart:js_interop';
 
 import '../../../models/screenset_event.dart';
 import '../models/profile.dart';
 
-/// The static interop class for the after validation event of the `Account.showScreenset` event stream.
+/// The extension type for the after validation event of the `Account.showScreenset` event stream.
 ///
 /// See: https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/413a5b7170b21014bbc5a10ce4041860.html?locale=en-US#onaftervalidation
 @JS()
 @anonymous
 @staticInterop
-class AfterValidationEvent {}
-
-/// This extension defines the static interop definition
-/// for the [AfterValidationEvent] class.
-extension AfterValidationEventExtension on AfterValidationEvent {
+extension type AfterValidationEvent(JSObject _) {
   /// The data object for the user. This will be empty if the user is not logged in.
-  external Object? get data;
+  external JSAny? get data;
 
   /// The name of the event.
   external String get eventName;
@@ -24,7 +19,8 @@ extension AfterValidationEventExtension on AfterValidationEvent {
   /// The ID of the form.
   external String get form;
 
-  /// The profile object for the user. This will be empty if the user is not logged in.
+  /// The profile object for the user.
+  /// This will be empty if the user is not logged in.
   external Profile? get profile;
 
   /// The name of the screen.
@@ -35,24 +31,26 @@ extension AfterValidationEventExtension on AfterValidationEvent {
   /// e.g., 'showLoginUI', 'showCommentsUI', etc.
   external String get source;
 
+  // TODO: This should be the subscriptions static interop type.
+
   /// The subscriptions data for the user. This will be empty if the user is not logged in.
-  external Object? get subscriptions;
+  external JSAny? get subscriptions;
 
   /// An object containing the data for any fields that failed validation.
-  external Object? get validationErrors;
+  external JSAny? get validationErrors;
 
   /// Serialize this event into a [ScreensetEvent].
   ScreensetEvent serialize() {
     return ScreensetEvent(
       eventName,
       <String, dynamic>{
-        'data': dartify(data),
+        'data': data.dartify(),
         'form': form,
         'profile': profile?.toMap(),
         'screen': screen,
         'source': source,
-        'subscriptions': dartify(subscriptions),
-        'validationErrors': dartify(validationErrors),
+        'subscriptions': subscriptions.dartify(),
+        'validationErrors': validationErrors.dartify(),
       },
     );
   }
