cf083f416477c82f44aca18e394c5cd54dcf0821
Navaron Bracke <brackenavaron@gmail.com>
2023-09-28 13:59:19 +0200

    convert field changed event to extension type

--- lib/src/web/static_interop/screenset_event/field_changed_event.dart
+++ lib/src/web/static_interop/screenset_event/field_changed_event.dart
@@ -1,18 +1,14 @@
-import 'package:js/js.dart';
+import 'dart:js_interop';
 
 import '../../../models/screenset_event.dart';
 
-/// The static interop class for the field changed event of the `Account.showScreenset` event stream.
+/// The extension type for the field changed event of the `Account.showScreenset` event stream.
 ///
 /// See: https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/413a5b7170b21014bbc5a10ce4041860.html?locale=en-US#onfieldchanged-event-data
 @JS()
 @anonymous
 @staticInterop
-class FieldChangedEvent {}
-
-/// This extension defines the static interop definition
-/// for the [FieldChangedEvent] class.
-extension FieldChangedEventExtension on FieldChangedEvent {
+extension type FieldChangedEvent(JSObject _) {
   /// The error message for the [field].
   external String? get errMsg;
 
