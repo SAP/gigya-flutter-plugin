7c4c81d871e8c7eb58c3e6f7c691c8ff8f325144
Navaron Bracke <brackenavaron@gmail.com>
2023-09-29 09:20:30 +0200

    convert init registration response to extension type

--- lib/src/web/static_interop/response/registration_response.dart
+++ lib/src/web/static_interop/response/registration_response.dart
@@ -1,18 +1,14 @@
-import 'package:js/js.dart';
+import 'dart:js_interop';
 
 import 'response.dart';
 
-/// The static interop class for the Gigya InitRegistration API response.
+/// The extension type for the Gigya InitRegistration API response.
 ///
 /// See also: https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/4136cef070b21014bbc5a10ce4041860.html?locale=en-US#response-example
 @JS()
 @anonymous
 @staticInterop
-class InitRegistrationResponse extends Response {}
-
-/// This extension defines the static interop definition
-/// for the [InitRegistrationResponse] class.
-extension InitRegistrationResponseExtension on InitRegistrationResponse {
+extension type InitRegistrationResponse(Response baseResponse) {
   /// The registration token for the registration.
   external String? get regToken;
 }
