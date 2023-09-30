de8ff8ca9a25bca015db983c26176996c83ba833
Navaron Bracke <brackenavaron@gmail.com>
2023-09-30 15:12:42 +0200

    convert account login id to extension type

--- lib/src/web/static_interop/models/account_login_id.dart
+++ lib/src/web/static_interop/models/account_login_id.dart
@@ -1,35 +1,31 @@
-import 'package:js/js.dart';
+import 'dart:js_interop';
 
-/// The static interop class for the login id of a user.
+/// The extension type for the login id of a user.
 @JS()
 @anonymous
 @staticInterop
-class AccountLoginId {}
-
-/// This extension defines the static interop definition
-/// for the [AccountLoginId] class.
-extension AccountLoginIdExtension on AccountLoginId {
+extension type AccountLoginId(JSObject _) {
   @JS('emails')
-  external List<dynamic> get _emails;
+  external JSArray get _emails;
 
   @JS('unverifiedEmails')
-  external List<dynamic> get _unverifiedEmails;
+  external JSArray get _unverifiedEmails;
 
   /// The list of verified email addresses of the user.
-  List<String> get emails => _emails.cast<String>();
-
-  /// The username of the user.
-  external String get username;
+  List<String> get emails => _emails.toDart.cast<String>();
 
   /// The list of unverified email addresses of the user.
-  List<String> get unverifiedEmails => _unverifiedEmails.cast<String>();
+  List<String> get unverifiedEmails => _unverifiedEmails.toDart.cast<String>();
+
+  /// The username of the user.
+  external String? get username;
 
   /// Convert this object into a map.
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'emails': emails,
-      'username': username,
       'unverifiedEmails': unverifiedEmails,
+      'username': username,
     };
   }
 }
