1f9f26ba2665f235bc2995339670a32346c30fba
Navaron Bracke <brackenavaron@gmail.com>
2023-10-01 19:31:29 +0200

    convert social connection namespace to extension types

--- lib/src/web/static_interop/parameters/social_connection.dart
+++ lib/src/web/static_interop/parameters/social_connection.dart
@@ -1,18 +1,21 @@
-import 'package:js/js.dart';
+import 'dart:js_interop';
 
 import '../response/social_connection_response.dart';
 
-/// This class represents the parameters for the `Socialize.addConnection` method.
+/// This extension type represents the parameters for the `Socialize.addConnection` method.
 ///
 /// See also: https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/417376f070b21014bbc5a10ce4041860.html#parameters
 @JS()
 @anonymous
 @staticInterop
-class AddSocialConnectionParameters {
+extension type AddSocialConnectionParameters._(JSObject _) implements JSObject {
   /// Create a new [AddSocialConnectionParameters] instance.
+  /// 
+  /// The [callback] function receives a [SocialConnectionResponse] as argument
+  /// and has [JSVoid] as return type.
   external factory AddSocialConnectionParameters({
     String? authFlow,
-    void Function(SocialConnectionResponse response) callback,
+    JSFunction callback,
     String? extraFields,
     String? facebookExtraPermissions,
     bool? forceAuthentication,
@@ -24,16 +27,19 @@ class AddSocialConnectionParameters {
   });
 }
 
-/// This class represents the parameters for the `Socialize.removeConnection` method.
+/// This extension type represents the parameters for the `Socialize.removeConnection` method.
 ///
 /// See also: https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/4177c60370b21014bbc5a10ce4041860.html#parameters
 @JS()
 @anonymous
 @staticInterop
-class RemoveSocialConnectionParameters {
+extension type RemoveSocialConnectionParameters._(JSObject _) implements JSObject {
   /// Create a new [RemoveSocialConnectionParameters] instance.
+  /// 
+  /// The [callback] function receives a [SocialConnectionResponse] as argument
+  /// and has [JSVoid] as return type.
   external factory RemoveSocialConnectionParameters({
-    void Function(SocialConnectionResponse response) callback,
+    JSFunction callback,
     String provider,
     bool? forceProvidersLogout,
     String? lastIdentityHandling,
