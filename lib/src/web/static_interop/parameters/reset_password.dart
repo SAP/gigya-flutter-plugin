93686e9b73659bccaf993a21c0a7b1e8e4d056dc
Navaron Bracke <brackenavaron@gmail.com>
2023-10-02 15:14:08 +0200

    convert reset password response & arguments to extension types

--- lib/src/web/static_interop/parameters/reset_password.dart
+++ lib/src/web/static_interop/parameters/reset_password.dart
@@ -1,19 +1,22 @@
-import 'package:js/js.dart';
+import 'dart:js_interop';
 
-/// This class represents the parameters for the `Accounts.resetPassword` method.
+/// This extension type represents the parameters for the `Accounts.resetPassword` method.
 @JS()
 @anonymous
 @staticInterop
-class ResetPasswordParameters {
+extension type ResetPasswordParameters._(JSObject _) implements JSObject {
   /// Create a new [ResetPasswordParameters] instance.
   external factory ResetPasswordParameters({
     String? email,
-    bool ignoreInterruptions,
+    bool? ignoreInterruptions,
     String? lang,
     String? loginID,
     String? newPassword,
     String? passwordResetToken,
     String? secretAnswer,
     String? securityFields,
+    // TODO: this parameter is not documented in the list at
+    // https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/c21d4e0445b84a779af1ad4868902c21.html#parameters
+    bool? sendEmail,
   });
 }
--- lib/src/web/static_interop/response/reset_password_response.dart
+++ lib/src/web/static_interop/response/reset_password_response.dart
@@ -1,39 +1,26 @@
 import 'dart:js_interop';
 
+import '../models/emails.dart';
 import 'response.dart';
 
-// TODO: fix the extension type
-// - String? time
-// - Emails? emails
-// - String? UID -> fix the JS annotation
-
-/// The static interop class for the Gigya Reset Password API response.
+/// The extension type for the Gigya Reset Password API response.
 ///
 /// See also: https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/c21d4e0445b84a779af1ad4868902c21.html#response-data
 @JS()
 @anonymous
 @staticInterop
-class ResetPasswordResponse extends Response {}
-
-/// This extension defines the static interop definition
-/// for the [ResetPasswordResponse] class.
-extension ResetPasswordResponseExtension on ResetPasswordResponse {
-  @JS('emails')
-  external Object? get _emails;
-
+extension type ResetPasswordResponse(Response baseResponse) {
   /// The email addresses belonging to the user.
-  Map<String, dynamic> get emails {
-    if (_emails.isUndefinedOrNull) {
-      return const <String, dynamic>{};
-    }
-
-    return (dartify(_emails) as Map<Object?, Object?>).cast<String, dynamic>();
-  }
+  external Emails? get emails;
 
   /// The token that should be used for the password reset in the password reset email.
   ///
-  /// This is null if the email is sent by the Gigya SDK.
+  /// This is null if `ResetPasswordParameters.sendEmail` was `false`.
   external String? get passwordResetToken;
+
+  /// The time of the response, in ISO 8601 format,
+  /// i.e. 2021-02-06T22:05:47.706Z.
+  external String? get time;
 
   /// The secret question for the password reset.
   ///
@@ -43,6 +30,16 @@ extension ResetPasswordResponseExtension on ResetPasswordResponse {
   /// The UID of the user whose password was changed.
   ///
   /// This is null if the password was not changed yet.
-  @JS('UID')
-  external String? get uid;
+  external String? get UID; // ignore: non_constant_identifier_names
+
+  /// Convert this response to a [Map].
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'emails': emails?.toMap(),
+      'passwordResetToken': passwordResetToken,
+      'time': time,
+      'secretQuestion': secretQuestion,
+      'UID': UID,
+    };
+  }  
 }
