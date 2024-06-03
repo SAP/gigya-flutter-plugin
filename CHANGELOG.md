92540823f9c4534df664e0b08225bbabfc4821b3
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2024-06-03 11:53:48 +0300

    Preparing version update 1.0.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 1.0.6
+1.0.6
+
+* Added "getAuthCode" interface used to obtain a session exchange code with WebSDK.
+
 # 1.0.5
 1.0.5
 
--- ios/Classes/GigyaSdkWrapper.swift
+++ ios/Classes/GigyaSdkWrapper.swift
@@ -50,7 +50,7 @@ public class GigyaSdkWrapper<T: GigyaAccountProtocol> :GigyaInstanceProtocol {
     
     init(accountSchema: T.Type) {
         // Initializing the Gigya SDK instance.
-        GigyaDefinitions.versionPrefix = "flutter_1.0.5_"
+        GigyaDefinitions.versionPrefix = "flutter_1.0.6_"
         sdk = Gigya.sharedInstance(accountSchema)
         GigyaAuth.shared.register(scheme: accountSchema)
     }
