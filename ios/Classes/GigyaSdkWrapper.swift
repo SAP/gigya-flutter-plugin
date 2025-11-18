97f41febd91e8e9c037d1aa7a8e0e14b8b51a207
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2025-11-18 16:56:27 +0200

    release: version 1.0.13

--- ios/Classes/GigyaSdkWrapper.swift
+++ ios/Classes/GigyaSdkWrapper.swift
@@ -67,7 +67,7 @@ public class GigyaSdkWrapper<T: GigyaAccountProtocol> :GigyaInstanceProtocol {
     
     init(accountSchema: T.Type) {
         // Initializing the Gigya SDK instance.
-        GigyaDefinitions.versionPrefix = "flutter_1.0.12_"
+        GigyaDefinitions.versionPrefix = "flutter_1.0.13_"
         sdk = Gigya.sharedInstance(accountSchema)
         GigyaAuth.shared.register(scheme: accountSchema)
     }
